1c6ba31d51836eb903f0301914a760af2b3f9327
Danilo Tadeu Orige Oliveira <daniloorige@gmail.com>
2025-11-27 22:59:19 -0300

    Load and PV profile using real data

--- dev/simulation.py
+++ dev/simulation.py
@@ -4,15 +4,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cvxpy as cp
 import control as ct
+from microgrid_data.data import load_microgrid_data
 
 
 ##------------------------------MPC MICROGRID SIMULATION------------------------------##
+
+########## Load Data
+data = load_microgrid_data()
+pv_power = data['pv_power']  # PV power array [W]
+load_power = data['load_power']  # Load power array [W]
+load_power = load_power*11
+grid_power = data['grid_power']  # Grid power array [W]
+
+########## Creating profiles for simulation
+# 2 days simulation
+
+pv_profile = np.hstack((pv_power, pv_power))  # PV generation profile [W]
+
+# plt.figure()
+# plt.plot(np.arange(0, 48, 0.25), pv_profile)
+# plt.title('PV Generation Profile over 2 Days')
+# plt.xlabel('Time [hours]')
+# plt.ylabel('PV Power [W]')
+# plt.grid()
+# plt.show()
+
+load_profile = np.hstack((load_power, load_power))  # Load profile [W]
+
+# plt.figure()
+# plt.plot(np.arange(0, 48, 0.25), load_profile)
+# plt.title('Load Profile over 2 Days')
+# plt.xlabel('Time [hours]')
+# plt.ylabel('Load Power [W]')
+# plt.grid()
+# plt.show()
+
 
 ########## Parameters
 
 # Battery parameters
 C_bat = [2 * 1000]  # Battery capacity [Wh]
-ch_bat = [0.9] # Charge efficiency
+ch_bat = [1] # Charge efficiency
 SoC_CI = [0]  # Initial state of charge [%]
 
 # Sampling parameters
@@ -50,16 +82,16 @@ tariff_sim = np.hstack((tariff_daily, tariff_daily))  # Tariff for the whole sim
 
 
 # Tariff plot
-tx_plot = np.arange(0, 24, ts/3600)
-plt.figure()
-plt.plot(tx_plot, tariff_daily, color = 'red')
-plt.xticks(np.arange(0, 26, 2))
-plt.yticks(np.arange(0, 1.5, 0.1))
-plt.title('Electricity Tariff - Florianopolis, SC, Brazil (2025)')
-plt.xlabel('Time [hours]')
-plt.ylabel('Price [R$/kWh]')
-plt.grid()
-plt.show()
+# tx_plot = np.arange(0, 24, ts/3600)
+# plt.figure()
+# plt.plot(tx_plot, tariff_daily, color = 'red')
+# plt.xticks(np.arange(0, 26, 2))
+# plt.yticks(np.arange(0, 1.5, 0.1))
+# plt.title('Electricity Tariff - Florianopolis, SC, Brazil (2025)')
+# plt.xlabel('Time [hours]')
+# plt.ylabel('Price [R$/kWh]')
+# plt.grid()
+# plt.show()
 
 # Load and PV generation profiles
 # Load profile [W]  
@@ -69,14 +101,16 @@ plt.show()
 
 # Baterry Model
 #SoC_a = previous SoC [Wh]
-K1 = (ts/3600)*100*ch_bat[0]/C_bat[0]   # Charging constant 
+K1 = 100*(ts/3600)*ch_bat[0]/C_bat[0]   # Charging constant 
+
 # ts/3600 = Sampling time in hours
 
 
 
 
+
 ## DMC Matrices for Battery SoC Control
-# Step response of the battery SoC to a step change in charging power
+# Step response of battery SoC to a step change in charging power
 
 step_test = 1 # [W]
 
@@ -84,21 +118,21 @@ SoC_coef = np.zeros(Nsim)
 SoC_coef[0] = 0  # Initial SoC for step response simulation
 
 for k in range(1, Nsim):
-        SoC = 0.999*SoC_coef[k-1] + K1 * step_test
+        SoC = SoC_coef[k-1] + K1 * step_test
         SoC = np.clip(SoC, 0, 100)
         SoC_coef[k] = SoC
 
-print(SoC_coef)
+
 SoC_x = np.arange(0, Nsim) / samples_hour  # Time vector in hours
 
 # Plot Battery SoC Step Response 
-plt.figure()
-plt.plot(SoC_x, SoC_coef)
-plt.title('Battery State of Charge over Time')
-plt.xlabel('Time [hours]')
-plt.ylabel('State of Charge [%]')
-plt.grid()
-plt.show()
+# plt.figure()
+# plt.plot(SoC_x, SoC_coef)
+# plt.title('Battery State of Charge over Time')
+# plt.xlabel('Time [hours]')
+# plt.ylabel('State of Charge [%]')
+# plt.grid()
+# plt.show()
 
 def G_matrix(Nu, N, g):
     G = []
@@ -109,19 +143,32 @@ def G_matrix(Nu, N, g):
     G = np.vstack(G).T
     return G
 G = G_matrix(Nu, Np, SoC_coef)
-print(G.shape)
 
-def H_matrix(N, Nss, g):
-    H = np.zeros((Nss, N))
-    for j in range(Nss):
-        for i in range(N):
-            if (j + i + 1) < Nss:
-                H[j, i] = g[j + i + 1] - g[j]
-            else:
-                H[j, i] = g[-1] - g[j]
-    return H.T
+###### Charging Tests 
 
-H = H_matrix(Np, Np, SoC_coef)
+SoCk = np.zeros(Nsim)
+SoCk[0] = 100 # Initial SoC
+
+
+#load consumption profile
+for k in range(1, Nsim):
+        SoC = SoCk[k-1] + K1*(-load_profile[k-1] + pv_profile[k-1]) 
+        SoC = np.clip(SoC, 0, 100)
+        SoCk[k] = SoC
+
+plt.figure()
+plt.plot(np.arange(0, t_sim, ts/3600), SoCk)
+plt.title('Battery State of Charge over Time - Open Loop')
+plt.xlabel('Time [hours]')
+plt.ylabel('State of Charge [%]')
+plt.grid()
+plt.show()
+
+
+
+
+
+
 
     
 
